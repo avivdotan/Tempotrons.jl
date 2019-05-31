97a5c88eb6a1d3e43fdea4228535d81c84eb0d4b
avivdotan <avivd220@gmail.com>
2019-06-01 01:48:13 +0300

    Docs format

--- src/Tempotrons.jl
+++ src/Tempotrons.jl
@@ -3,6 +3,7 @@ An implementation of The Binary Tempotron [1] and the Multi-Spike Tempotron[2].
 
 # References
 [1] [Gütig, R., & Sompolinsky, H. (2006). The tempotron: a neuron that learns spike timing–based decisions. Nature neuroscience, 9(3), 420.](https://www.nature.com/articles/nn1643)
+
 [2] [Gütig, R. (2016). Spiking neurons can discover predictive features by aggregate-label learning. Science, 351(6277), aab4113.](https://science.sciencemag.org/content/351/6277/aab4113)
 """
 module Tempotrons
