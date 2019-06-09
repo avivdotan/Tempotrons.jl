cfc4ab3a413b938554001ee56401937ed173a82d
avivdotan <avivd220@gmail.com>
2019-06-10 02:07:23 +0300

    Package
    * Turned into an actual julia package. 
    * The installation process is still to be tested.

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 # Tempotrons.jl
-An implementation of The Binary Tempotron [[1](#references)] and the Multi-Spike Tempotron[[2](#references)]. See the [`TestBinary.jl`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/TestBinary.jl)/[`TestMultiSpike.jl`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/TestMultiSpike.jl) files or the (equivalent) Jupyter Notebooks [`BinaryTempotron.ipynb`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/BinaryTempotron.ipynb)/[`MultiSpikeTempotron.ipynb`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/MultiSpikeTempotron.ipynb) for simple use-cases.
+An implementation of The Binary Tempotron [[1](#references)] and the Multi-Spike Tempotron[[2](#references)]. See the [`TestBinary.jl`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/test/TestBinary.jl)/[`TestMultiSpike.jl`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/test/TestMultiSpike.jl) files or the (equivalent) Jupyter Notebooks [`BinaryTempotron.ipynb`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/BinaryTempotron.ipynb)/[`MultiSpikeTempotron.ipynb`](https://github.com/bci4cpl/Tempotrons.jl/blob/master/MultiSpikeTempotron.ipynb) for simple use-cases.
 
-## Progress
-- [x] The Binary Tempotron is fully implemented.
-- [x] The Multi-Spike Tempotron is fully implemented.
-
-## Dependencies
-* Julia >1.0.0
-* [Roots.jl](https://github.com/JuliaMath/Roots.jl)
-* [ForwardDiff.jl](https://github.com/JuliaDiff/ForwardDiff.jl) (to be removed)
-* [Distributions.jl](https://github.com/JuliaStats/Distributions.jl) (for Input Generation module)
-* [Plots.jl](juliaplots.org) (for Plots module)
-* [PyPlot.jl](https://github.com/JuliaPy/PyPlot.jl) (for demos' plots)
+## Installation
+First, install Julia >1.1.0.
+In the Julia REPL, press `]` to enter package mode then run:
+```console
+pkg> add https://github.com/bci4cpl/Tempotrons.jl.git
+```
 
 ## References
 [1] [Gütig, R., & Sompolinsky, H. (2006). The tempotron: a neuron that learns spike timing–based decisions. Nature neuroscience, 9(3), 420.](https://www.nature.com/articles/nn1643)
--- test/TestBinary.jl
+++ test/TestBinary.jl
@@ -1,5 +1,4 @@
 # Imports
-push!(LOAD_PATH, abspath("../"))
 using Tempotrons
 using Tempotrons.InputGen
 using Tempotrons.Plots
@@ -11,21 +10,20 @@ N = 10
 T = 500
 dt = 0.1
 t = collect(0:dt:T)
+λ = 0.01
+opt = SGD(λ)
 ν = 3
-λ = 0.1
-opt = RMSprop(λ)
 n_samples = 10
-n_classes = 5
-n_steps = 1000
+n_steps = 5000
 tmp = Tempotron(N = N)
 
 # Generate input samples
 base_samples = [[PoissonSpikeTrain(ν = ν, T = T)
                  for i = 1:N]
-                for j = 1:n_classes]
+                for j = 1:2]
 samples = [(x = [SpikeJitter(s, T = T, σ = 5)
-                 for s ∈ base_samples[n_classes*(j - 1)÷n_samples + 1]],
-            y = n_classes*(j - 1)÷n_samples)
+                 for s ∈ base_samples[2(j-1)÷n_samples + 1]],
+            y = Bool(2(j-1)÷n_samples))
            for j = 1:n_samples]
 
 # Get the tempotron's output before training
@@ -33,7 +31,6 @@ out_b = [tmp(s.x, t = t)[2] for s ∈ samples]
 
 # Train the tempotron
 @time for i = 1:n_steps
-    @debug "Sample: $i"
     s = rand(samples)
     Train!(tmp, s.x, s.y, optimizer = opt)
 end
@@ -41,29 +38,13 @@ end
 # Get the tempotron's output after training
 out_a = [tmp(s.x, t = t)[2] for s ∈ samples]
 
-# Prepare to plot
-pyplot(size = (700, 1000))
-C(g::ColorGradient) = RGB[g[z]
-    for z = range(0, stop = 1, length = n_classes)]
-clibrary(:misc)
-g = :rainbow
-cols = cgrad(g) |> C
-
 # Plots
-inp_plots = [PlotInputs(s.x, T_max = T, color = cols[s.y + 1])
-  for s ∈ samples]
+pyplot(size = (700, 1000))
+inp_plots = [PlotInputs(s.x, T_max = T, color = (s.y ? :red : :blue))
+             for s ∈ samples]
 train_plots = [PlotPotential(tmp, out_b = out_b[i], out_a = out_a[i],
-                             t = t, color = cols[samples[i].y + 1])
-                for i = 1:length(samples)]
+                             t = t, color = (samples[i].y ? :red : :blue))
+               for i = 1:length(samples)]
 ps = vcat(reshape(inp_plots, 1, :), reshape(train_plots, 1, :))
 p = plot(ps[:]..., layout = (length(inp_plots), 2))
 display(p)
-
-# Save plots
-filename(i) = "Results\\results" * string(i) * ".png"
-let i = 0
-    while isfile(filename(i))
-        i += 1
-    end
-    savefig(p, filename(i))
-end
