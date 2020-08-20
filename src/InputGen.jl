c1b655e8a2fb7e4dd5901120f87259fe445adbab
avivdotan <avivd220@gmail.com>
2020-08-20 14:53:58 +0300

    "implicit" conversions

--- src/InputGen.jl
+++ src/InputGen.jl
@@ -62,8 +62,8 @@ function spikes_jitter(si::SpikesInput{T1,N};
     end
     return SpikesInput(out, duration = T)
 end
-function spikes_jitter(si::Array{Array{T,1},1};
-                kwargs...) where {T}
+function spikes_jitter(si::Array{Array{T,1},1} where T;
+                kwargs...)
     return spikes_jitter(convert(SpikesInput, si); kwargs...)
 end
 
@@ -96,8 +96,8 @@ function spikes_jitter!(si::SpikesInput{T1,N};
     si.from = max(si.to, dur.to)
     return
 end
-function spikes_jitter!(si::Array{Array{T,1},1};
-                kwargs...) where {T}
+function spikes_jitter!(si::Array{Array{T,1},1} where T;
+                kwargs...)
     spikes_jitter!(convert(SpikesInput, si); kwargs...)
     return
 end
