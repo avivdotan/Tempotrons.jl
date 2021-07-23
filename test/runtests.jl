b70f2f66a85e34481d92f809e5c82ee3b1e60ae6
avivdotan <avivd220@gmail.com>
2021-07-24 00:18:36 +0300

    minor changes, update to julia 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,7 +23,7 @@ function test_convergence(; tmp = Tempotron(10), teacher_type = Bool, T = 500,
     base_samples = [poisson_spikes_input(N, ν = ν, T = T) for j = 1:n_classes]
     samples = [(x = spikes_jitter(base_samples[n_classes * (j - 1) ÷ n_samples + 1]),
                 y = teacher_type <: SpikesInput ?
-                    poisson_spikes_input(1, ν = 3, T = T) :
+                    poisson_spikes_input(1, ν = ν, T = T) :
                     min(n_classes * (j - 1) ÷ n_samples, typemax(teacher_type)))
                for j = 1:n_samples]
 
